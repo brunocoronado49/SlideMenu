81255924803ea92b2abaad1fdb23a4ede5f2e08d
brunocoronado49 <jofrancoracor@gmail.com>
2021-09-27 08:38:19 -0500

    Initial Commit

---
+++ SlideMenu/SlideMenuApp.swift
@@ -0,0 +1,17 @@
+//
+//  SlideMenuApp.swift
+//  SlideMenu
+//
+//  Created by Bruno Coronado on 27/09/21.
+//
+
+import SwiftUI
+
+@main
+struct SlideMenuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
